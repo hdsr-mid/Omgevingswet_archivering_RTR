613fe36cbd2051db38ee633c0a4468aa3b82efb3
Jakob Povel <jakobpovel@protonmail.com>
2024-03-20 14:24:31 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Python script om de status van de Registratie Toepasbare Regels naar .xlsx formaat te schrijven. 
+
+- Voeg de productie en pre API-keys toe aan de files in de volgende folder, deze keys kun je aanvragen bij [Iplo](https://aandeslagmetdeomgevingswet.nl/ontwikkelaarsportaal/api-register/api/omgevingsdocument-toepasbaar-opvragen/).
+```
+    code/Prod_API-key.txt
+    code/Pre_API-key.txt
+```
+- Open code/log_RTR_status_via_API.py en zorg dat je root directory gelijk is aan het pad van de github repository
+- Om bij de juiste activiteiten aan te komen moet je het bestand 'Prod_Activiteiten_Waterschapsverordening.txt' of 'Pre_Activiteiten_Waterschapsverordening.txt' vullen met de gewenste activiteiten en hun meta data. Voor de activiteit stijger aanleggen ziet dat er bij HDSR zo uit:
+```
+steiger aanleggen	nl.imow-ws0636.activiteit.BeperkingengebiedAct	nl.imow-ws0636.activiteit.SteigerAanleggen	Zorgplicht	Beperkingengebiedactiviteit	4.8.1	HDSR
+```
+- Automatisch wordt de huidige datum gebruikt voor het draaien van het script, in de RTR kun je ook terug in de tijd, of in de toekomst zoeken. Je kunt de variabele 'retrieval_date' aanpassen naar de gewenste datum bijvoorbeeld "01-01-2024".
+```
+retrieval_date = "01-01-2024"
+```
+
---
+++ code/log_RTR_status_via_API.py
@@ -0,0 +1,162 @@
+import os
+from datetime import datetime
+import requests
+import xlsxwriter
+
+root            = "G:\\DSO_Github"
+enviroment      = "Pre"
+api_key_file    = f"code/{enviroment}_API-key.txt"
+activities_file = f"data/{enviroment}_Activiteiten_Waterschapsverordening.txt"
+retrieval_date  = datetime.now().strftime("%d-%m-%Y")
+
+class APIUtility:
+    def __init__(self, root_directory, api_key_file, activities_file, retrieval_date):
+        self.root_directory = root_directory
+        os.chdir(self.root_directory)
+        self.api_key          = self.load_api_key(api_key_file)
+        self.headers          = {'Accept': 'application/hal+json', 'x-api-key': self.api_key}
+        self.activities_file  = activities_file
+        self.retrieval_date   = retrieval_date
+        self.base_url         = self.determine_base_url(enviroment)
+        self.urns             = self.load_activities(activities_file)
+        self.setup_excel()
+
+    @staticmethod
+    def load_api_key(api_key_file):
+        with open(api_key_file) as key_file:
+            return key_file.read().strip()
+
+    @staticmethod
+    def load_activities(activities_file):
+        urns = []
+        with open(activities_file) as file:
+            for line in file:
+                activity = line.strip().split("\t")
+                if len(activity) < 8:
+                    urns.append(activity)
+        return urns
+
+    def setup_excel(self):
+        document_name   = f"Waterschapsverordening_RTR_{enviroment}_status_{self.retrieval_date}.xlsx"
+        self.workbook   = xlsxwriter.Workbook(f"log/{document_name}")
+        self.worksheet  = self.workbook.add_worksheet()
+        self.prepare_worksheet()
+
+    def prepare_worksheet(self):
+        header_format = self.workbook.add_format({
+            'bg_color': '#DDDDDD',
+            'text_wrap': True,
+            'align': 'left',
+            'valign': 'top',
+            'bold': True,
+            'border': True,
+        })
+        self.cell_format = self.workbook.add_format({
+            'bg_color': 'white',
+            'text_wrap': False,
+            'align': 'left',
+            'valign': 'top',
+            'bold': False,
+            'border': True,
+        })
+        headers = [
+            "Activiteit                   ",
+            "Uri",
+            "Activiteiten Groep",
+            "Regel",
+            "Werkzaamheden",
+            "Wijziging Conclusie",
+            "Wijziging Melding",
+            "Wijziging Aanvraag vergunning",
+            "Wijziging Informatie",
+        ]
+        self.worksheet.write_row('A1', headers, header_format)
+        for i, header in enumerate(headers, 1):
+            self.worksheet.set_column(i - 1, i - 1, max(10, len(header)) + 2)
+
+    def retrieve_and_log_data(self):
+        with requests.Session() as session:
+            for row, activity in enumerate(self.urns, 2):
+                self.process_activity(session, activity, row)
+        self.workbook.close()
+
+    def process_activity(self, session, activity, row):
+        name, _, uri, _, activity_group, rule_reference, _ = activity
+        response_json = self.fetch_activity_data(session, uri)
+        if response_json:
+            self.log_activity_data(
+                session, row, name, uri, activity_group, rule_reference, response_json
+            )
+
+    def fetch_activity_data(self, session, uri):
+        url = self.compose_activity_url(uri)
+        response = session.get(url, headers=self.headers)
+        if response.ok:
+            return response.json()
+        print(f"Error fetching data for URI {uri}: {response.status_code}")
+        return None
+
+    @staticmethod
+    def extract_werkzaamheden(data):
+        werkzaamheden_list = []
+        if "werkzaamheden" in data["_links"]:
+            for werkzaamheid in data["_links"]["werkzaamheden"]:
+                extracted_id = werkzaamheid["href"].split("/")[(-1)]
+                werkzaamheden_list.append(extracted_id)
+        return [', '.join(werkzaamheden_list)] if werkzaamheden_list else [""]
+
+    def fetch_and_process_changes(self, session, data):
+        changes = ["", "", "", ""]
+        if "regelBeheerObjecten" in data:
+            for obj in data["regelBeheerObjecten"]:
+                objectType = obj["typering"]
+                if objectType == "Indieningsvereisten":
+                    objectType = obj["toestemming"]["waarde"]
+                functionalStructRef = obj["functioneleStructuurRef"]
+                lastChanged = self.fetch_last_changed_date(session, functionalStructRef)
+                if objectType in {"Conclusie", "Melding", "Aanvraag vergunning", "Informatie"}:
+                    index = ["Conclusie", "Melding", "Aanvraag vergunning", "Informatie"].index(
+                        objectType
+                    )
+                    changes[index] = lastChanged
+        return changes
+
+    def fetch_last_changed_date(self, session, functionalStructRef):
+        url = self.compose_regel_beheer_object_url(functionalStructRef)
+        response = session.get(url, headers=self.headers)
+        if response.ok:
+            data = response.json()
+            embedded = data.get('_embedded', {})
+            toepasbareRegels = embedded.get('toepasbareRegels', [])
+            if toepasbareRegels:
+                return toepasbareRegels[0].get("laatsteWijzigingDatum", "")
+        return ""
+
+    @staticmethod
+    def determine_base_url(env):
+        if env == "Prod":
+            return "https://service.omgevingswet.overheid.nl/publiek/toepasbare-regels/api"
+        if env == "Pre":
+            return "https://service.pre.omgevingswet.overheid.nl/publiek/toepasbare-regels/api"
+        raise ValueError("Invalid environment specified")
+
+    def compose_activity_url(self, uri):
+        return f"{self.base_url}/rtrgegevens/v2/activiteiten/{uri}?datum={self.retrieval_date}"
+
+    def compose_regel_beheer_object_url(self, functionalStructRef):
+        return f"{self.base_url}/toepasbareregelsuitvoerengegevens/v1/toepasbareRegels?functioneleStructuurRef={functionalStructRef}&datum={self.retrieval_date}"
+
+    def log_activity_data(self, session, row, name, uri, activity_group, rule_reference, data):
+        werkzaamheden = self.extract_werkzaamheden(data)
+        changes = self.fetch_and_process_changes(session, data)
+        data_to_write = [name, uri, activity_group, rule_reference] + werkzaamheden + changes
+        self.worksheet.write_row(row - 1, 0, data_to_write, self.cell_format)
+
+        
+
+def main():
+    api_utility = APIUtility(root, api_key_file, activities_file, retrieval_date)
+    api_utility.retrieve_and_log_data()
+
+if __name__ == "__main__":
+    main()
