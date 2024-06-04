2e16a08ee9d33d74fb1b3e3b7b775bea7e29cbe2
CorrelateVisuals <jakobpovel@protonmail.com>
2024-06-04 10:35:25 +0200

    +

--- code/powerbi.py
+++ code/powerbi.py
@@ -1,13 +1,21 @@
 import pandas as pd
 
-excel = pd.read_excel(r"D:\HDSR\Github\waterschapsverordening_log_RTR_status\data\A1. Welke activiteiten zijn gewijzigd PROD.xlsx") 
+# Load the data from Excel files
+urn_excel = pd.read_excel(r"D:\HDSR\Github\waterschapsverordening_log_RTR_status\data\A1. Welke activiteiten zijn gewijzigd PROD.xlsx")
+location_excel = pd.read_excel(r"D:\HDSR\Github\waterschapsverordening_log_RTR_status\data\A2. Welke locaties worden er gebruikt PROD.xlsx")
 
-urns = pd.DataFrame(excel, columns=["URN", "Bestuursorgaan"])
+# Extract relevant columns into DataFrames
+urns = pd.DataFrame(urn_excel, columns=["Bestuursorgaan", "omschrijving", "URN"])
+locations = pd.DataFrame(location_excel, columns=["Bestuursorgaan", "omschrijving", "noemer", "identificatie"])
 
-urn_per_bestuursorgaan = {}
+# Sort the DataFrames by 'Bestuursorgaan'
+urns_sorted = urns.sort_values(by="Bestuursorgaan")
+locations_sorted = locations.sort_values(by="Bestuursorgaan")
 
-#for urn in urns:
+# Ensure row-wise data stays intact by resetting the index
+urns_sorted.reset_index(drop=True, inplace=True)
+locations_sorted.reset_index(drop=True, inplace=True)
 
-print(urns["Waterschap Limburg"])
-
-#print(urns)
+# Print the sorted DataFrames
+print(urns_sorted)
+print(locations_sorted)
