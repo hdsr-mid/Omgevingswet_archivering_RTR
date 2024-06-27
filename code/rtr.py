e6d3ab70f2a5835a35bfbfdf2d5af388f33c750f
CorrelateVisuals <jakobpovel@protonmail.com>
2024-06-27 19:13:49 +0200

    cleanup

--- code/rtr.py
+++ code/rtr.py
@@ -34,20 +34,6 @@ class RTR:
         self.excel_handler = None
 
     @staticmethod
-    def parse_command_line_arguments():
-        parser = argparse.ArgumentParser(description="Process some environment settings and actions.")
-        parser.add_argument('--env', type=str, default="prod", choices=['prod', 'pre'],
-                            help='Environment setting: prod (default) or pre.')
-        parser.add_argument('--date', type=str, default=datetime.now().strftime("%d-%m-%Y"),
-                            help='Date in the format dd-mm-yyyy, default is today\'s date.')
-        parser.add_argument('--sttr', action='store_true',
-                            help='Flag to archive STTR files in .xml')
-        parser.add_argument('--location', action='store_true',
-                            help='Flag to archive werkingsgebieden per activity to .txt')
-        args = parser.parse_args()
-        return args
-
-    @staticmethod
     def load_api_key(api_key_file):
         with open(api_key_file) as key_file:
             return key_file.read().strip()
